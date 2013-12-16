8889d7357dc7722f3f452c67320e694950360345
bboyarskiy <boris@Boriss-MacBook-Pro.local>
2013-12-16 19:30:22 +0100

    scroollen zum anker hinzugefügt

--- js/script.js
+++ js/script.js
@@ -3,11 +3,27 @@ $(document).ready(function(){
     // Cache the Window object
     $window = $(window);
 
+
+            // Alle internen Links auswählen
+        $('a[href*=#]').bind("click", function(event) {
+            // Standard Verhalten unterdrücken
+            event.preventDefault();
+            // Linkziel in Variable schreiben
+            var ziel = $(this).attr("href");
+            //Scrollen der Seite animieren, body benötigt für Safari
+            $('html,body').animate({
+                //Zum Ziel scrollen (Variable)
+                scrollTop: $(ziel).offset().top
+            // Dauer der Animation und Callbackfunktion die nach der Animation aufgerufen wird, sie stellt das Standardverhalten wieder her und ergänzt die URL
+            }, 1000 , function (){location.hash = ziel;});
+            return false;
+        });
+
+
     $('section[data-type="background"]').each(function(){
         var $bgobj = $(this); // assigning the object
 
         $(window).scroll(function() {
-
 
             // Scroll the background at var speed
             // the yPos is a negative value because we're scrolling it UP!
@@ -22,5 +38,7 @@ $(document).ready(function(){
         }); // window scroll Ends
 
     });
+
+
 
 });
