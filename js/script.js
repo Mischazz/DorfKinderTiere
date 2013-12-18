ddaf47bc8f5124edd964c9f0339d3b63c2fb1e18
Slin Kawon <nowaknils@aol.com>
2013-12-18 16:19:27 +0100

    create json and sections

--- js/script.js
+++ js/script.js
@@ -1,20 +1,39 @@
-$(document).ready(function(){
+$(document).ready(function () {
+
+
+    $.getJSON( "content.json", function( data ) {
+
+        console.log(data["Andere über Uns"]);
+
+    });
+
+    if ($(window).scroll) {
+
+        $('.footer').hide();
+    }
 
     // Cache the Window object
     $window = $(window);
 
 
-    $('section[data-type="background"]').each(function(){
+    $('section[data-type="background"]').each(function () {
         var $bgobj = $(this); // assigning the object
 
-        $(window).scroll(function() {
+        $(window).scroll(function () {
+            var check = 1
+            if (check === 1) {
+
+                $('.footer').hide();
+
+            }
+
 
             // Scroll the background at var speed
             // the yPos is a negative value because we're scrolling it UP!
             var yPos = -($window.scrollTop() / $bgobj.data('speed'));
 
             // Put together our final background position
-            var coords = '50% '+ yPos + 'px';
+            var coords = '50% ' + yPos + 'px';
 
             // Move the background
             $bgobj.css({ backgroundPosition: coords });
@@ -24,7 +43,6 @@ $(document).ready(function(){
     });
 
 
-    
     // Automatisches scrollen zur nächsten Section
     //
 
@@ -46,9 +64,9 @@ $(document).ready(function(){
     //         }
 
     //     });
-            
+
     //         if (st > lastScrollTop){
-            
+
     //         } else {
     //           // upscroll code
     //         }
@@ -56,26 +74,24 @@ $(document).ready(function(){
     // });
 
 
-
     //
     // Scrollen zu dem Ankerpunkt
     //
 
-    $('a[href*=#]').bind("click", function(event) {
+    $('a[href*=#]').bind("click", function (event) {
         // Standard Verhalten unterdrücken
-        event.preventDefault();    
+        event.preventDefault();
         var ziel = $(this).attr("href");
-        
+
         $('html,body').animate({
-        
+
             scrollTop: $(ziel).offset().top
             // Dauer der Animation und Callbackfunktion die nach der Animation aufgerufen wird, sie stellt das Standardverhalten wieder her und ergänzt die URL
-        }, 1000 , function (){location.hash = ziel;});
+        }, 1000, function () {
+            location.hash = ziel;
+        });
         return false;
     });
-
-
-
 
 
 });
