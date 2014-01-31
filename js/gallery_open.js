7e78fb07ca707b81dd2697653b38bf2533cbbc98
Slin Kawon <nowaknils@aol.com>
2014-01-31 18:04:03 +0100

    implement image json logic

--- js/gallery_open.js
+++ js/gallery_open.js
@@ -1,190 +1,237 @@
 $(document).ready(function () {
 
-	var $previewWidth;
+    var $previewWidth;
 
-	$("#closeP").hide();
-
-
-	$(".album").click(function(){
-
-		if($(".preview").height() == 0){
-			$(".preview").animate({height: "350px"}, 2000);
-		};
-
-			//Check if there are some images already and remove them
-			removeImg();
-
-			$("#closeP").show();
-			var elId = $(this).attr('id');
-
-			// Album1
-			if(elId == 'album1'){
-
-				$.ajax({
-					url: '/DorfKinderTiere/images/album1',
-					type: 'GET',
-					success: function(data){
-						
-						
-					}
-				})
-				.done(function(data) {
+    $("#closeP").hide();
 
 
+    $(".album").click(function () {
 
-					// var count = 0;
-					// $(data).find('ul li a').each(function(){
-					// 	count++;
-					// })
-					// console.log("count "+count);
+        if ($(".preview").height() == 0) {
+            $(".preview").animate({height: "350px"}, 2000);
+        }
+        ;
 
-					// var count = $(this).size();
-					// console.log("count ="+count);
+        //Check if there are some images already and remove them
+        removeImg();
 
-                      
+        $("#closeP").show();
+        var elId = $(this).attr('id');
 
-					var $previewWidth = 0;
-					var count = 0
-                        $(data).find('a').each(function(){
-                            count++;
-                            var name = $(this).attr('href');
-                            
-                            $(".preview").append('<img class="previewImg" src=/DorfKinderTiere/images/album1/'+name+'>');
-                        })
-                        console.log(count);
-                        console.log(data);
-					// for (var i = 1; i < count; i++) {
-						
-						
-					// };
+        // Album1
+        if (elId == 'album1') {
 
-					$(".previewImg").first().hide();
-					$(".previewImg").each(function() {
-						$previewWidth += $(this).width();	
-					});
+            $.ajax({
+                dataType: "json",
+                url: 'image.json',
+                type: 'GET',
+                success: function (data) {
 
-					$(".preview").css({
-						width: $previewWidth+'px'
-					});
-					console.log("success");
-				})
-				.fail(function() {
-					console.log("error");
-				})
-				.always(function() {
-					console.log("complete");
-				});
 
-				
-			}
+                }
+            })
+                .done(function (data) {
 
-			// Album2
-			if(elId == 'album2'){
-				$.ajax({
-					url: '/DorfKinderTiere/images/album2',
-					type: 'GET',
-					success: function(data){
+                    _.each(data, function (val, key) {
 
-						
-					}
-				})
-				.done(function(data) {
-					
-					var count = $("ul li a").length;
-					console.log("count ="+count);
-					for (var i = 1; i < count; i++) {
-						$(".preview").append('<img class="previewImg" src=/DorfKinderTiere/images/album2/'+i+'.jpg>')
-					};
-					
-					$(".previewImg").each(function() {
-						$previewWidth += $(this).width();	
-					});
+                        if (key === 'album1') {
 
-					$(".preview").css({
-						width: $previewWidth+'px'
-					});
-					console.log(data);
-					console.log("success");
-				})
-				.fail(function() {
-					console.log("error");
-				})
-				.always(function() {
-					console.log("complete");
-				});
+                            _.each(data["album1"], function (value, key) {
 
-			}
+                                $(".preview").append('<img class="previewImg" src=/DorfKinderTiere/images/album1/' + value + '>');
 
-			// Album3
-			if(elId == 'album3'){
-				$.ajax({
-					url: '/DorfKinderTiere/images/album3',
-					type: 'GET',
-					success: function(data){
+                            })
 
-						
-					}
-				})
-				.done(function(data) {
-					var count = $("ul li a").length;
-					for (var i = 1; i < count; i++) {
-						$(".preview").append('<img class="previewImg" src=/DorfKinderTiere/images/album3/'+i+'.jpg>')
-					};
-					$("a").attr('href');
-					
-					console.log("success");
-				})
-				.fail(function() {
-					console.log("error");
-				})
-				.always(function() {
-					console.log("complete");
-				});
-			}
-			// Album4
-			if(elId == 'album4'){
-				$.ajax({
-					url: '/DorfKinderTiere/images/album4',
-					type: 'GET',
-					success: function(data){
+                        }
 
-						
-					}
-				})
-				.done(function(data) {
-					var count = $("ul li a").length;
-					for (var i = 1; i < count; i++) {
-						$(".preview").append('<img class="previewImg" src=/DorfKinderTiere/images/album4/'+i+'.jpg>')
-					};
-					$("a").attr('href');
-					
-					console.log("success");
-				})
-				.fail(function() {
-					console.log("error");
-				})
-				.always(function() {
-					console.log("complete");
-				});
-			}
 
-	});
+                    });
 
-	$("#closeP").click(function(event) {
-		$(".preview").animate({height: "0px"}, 2000);
-		$(".previewImg").fadeOut(1000);
-		setTimeout(function() {
-			removeImg();
-		}, 2000); 
-	});
+
+                    var $previewWidth = 0;
+
+
+                    $(".previewImg").each(function () {
+                        $previewWidth += $(this).width();
+                    });
+
+                    $(".preview").css({
+                        width: $previewWidth + 'px'
+                    });
+                    console.log("success");
+                })
+                .fail(function () {
+                    console.log("error");
+                })
+                .always(function () {
+                    console.log("complete");
+                });
+
+
+        }
+
+        // Album2
+        if (elId == 'album2') {
+            $.ajax({
+                url: 'image.json',
+                dataType: "json",
+                type: 'GET',
+                success: function (data) {
+
+
+                }
+            })
+                .done(function (data) {
+
+                    _.each(data, function (val, key) {
+
+                        if (key === 'album2') {
+
+                            _.each(data["album2"], function (value, key) {
+
+                                $(".preview").append('<img class="previewImg" src=/DorfKinderTiere/images/album2/' + value + '>');
+
+                            })
+
+                        }
+
+
+                    });
+
+
+                    var $previewWidth = 0;
+
+                    $(".previewImg").each(function () {
+                        $previewWidth += $(this).width();
+                    });
+
+                    $(".preview").css({
+                        width: $previewWidth + 'px'
+                    });
+                    console.log(data);
+                    console.log("success");
+                })
+                .fail(function () {
+                    console.log("error");
+                })
+                .always(function () {
+                    console.log("complete");
+                });
+
+        }
+
+        // Album3
+        if (elId == 'album3') {
+            $.ajax({
+                url: 'image.json',
+                dataType: "json",
+
+                type: 'GET',
+                success: function (data) {
+
+
+                }
+            })
+                .done(function (data) {
+
+                    _.each(data, function (val, key) {
+
+                        if (key === 'album3') {
+
+                            _.each(data["album3"], function (value, key) {
+
+                                $(".preview").append('<img class="previewImg" src=/DorfKinderTiere/images/album3/' + value + '>');
+
+                            })
+
+                        }
+
+
+                    });
+
+                    var $previewWidth = 0;
+
+                    $(".previewImg").each(function () {
+                        $previewWidth += $(this).width();
+                    });
+
+                    $(".preview").css({
+                        width: $previewWidth + 'px'
+                    });
+                    console.log("success");
+                })
+                .fail(function () {
+                    console.log("error");
+                })
+                .always(function () {
+                    console.log("complete");
+                });
+        }
+        // Album4
+        if (elId == 'album4') {
+            $.ajax({
+                url: 'image.json',
+                dataType:'json',
+                type: 'GET',
+                success: function (data) {
+
+
+                }
+            })
+                .done(function (data) {
+                    _.each(data, function (val, key) {
+
+                        if (key === 'album4') {
+
+                            _.each(data["album4"], function (value, key) {
+
+                                $(".preview").append('<img class="previewImg" src=/DorfKinderTiere/images/album4/' + value + '>');
+
+                            })
+
+                        }
+
+
+                    });
+
+
+                    var $previewWidth = 0;
+
+                    $(".previewImg").each(function () {
+                        $previewWidth += $(this).width();
+                    });
+
+                    $(".preview").css({
+                        width: $previewWidth + 'px'
+                    });
+                    console.log("success");
+                })
+                .fail(function () {
+                    console.log("error");
+                })
+                .always(function () {
+                    console.log("complete");
+                });
+        }
+
+    });
+
+    $("#closeP").click(function (event) {
+        $(".preview").animate({height: "0px"}, 2000);
+        $(".previewImg").fadeOut(1000);
+        setTimeout(function () {
+            removeImg();
+        }, 2000);
+    });
 
 
 });
 
-	//Clear Preview
-	function removeImg(){
-		if ($(".previewImg").length =! 0) {
-			$(".previewImg").remove();
-		};
+//Clear Preview
+function removeImg() {
+    if ($(".previewImg").length = !0) {
+        $(".previewImg").remove();
+    }
+    ;
 
-	};
+};
